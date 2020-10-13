4cfb55b6e86302b5a628a03f695486e25107c9c5
shashank reddy <shashankpuli123@gmail.com>
2020-10-13 23:35:41 +0530

    Updates

---
+++ precache-manifest.d5a13698081011f2fc17a6825eac335d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f033d11eb947487649baa43815fae4b",
+    "url": "/react-final-project-with-bootstrap-reactstrap/index.html"
+  },
+  {
+    "revision": "5956d2948cbfa4f9952f",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/css/2.b3717b2c.chunk.css"
+  },
+  {
+    "revision": "03406c63eaf97ac13725",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/css/main.cefeeae2.chunk.css"
+  },
+  {
+    "revision": "5956d2948cbfa4f9952f",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/js/2.68a67834.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/js/2.68a67834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03406c63eaf97ac13725",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/js/main.4a12d8ba.chunk.js"
+  },
+  {
+    "revision": "35f461d91cd4e755e905",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/js/runtime-main.8546b38b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-final-project-with-bootstrap-reactstrap/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
